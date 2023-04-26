c9cd61b1ac57a61a4a4d163b797cdf37091e88f5
Blanco0420 <sachajamesterry@gmail.com>
2023-04-26 18:20:08 +0100

    Better css layout

--- script.js
+++ script.js
@@ -5,10 +5,53 @@
 //     element.focus();
 // }
 $(document).ready(function() {
-	$('.popup') >//FIXME hide all but button.hide();
-	$('.popup') > $('button').click(function(){
-		$(this).next(".child").toggle();
+	$(".popup button").click(function() {
+		$(this).parent().find("div").toggle();
 	});
+	// When the user scrolls the page, execute myFunction
+	// window.onscroll = function() {addSticky()};
+
+	// // Get the navbar
+	// var navbar = document.getElementById("topnav");
+
+	// // Get the offset position of the navbar
+	// var sticky = navbar.offsetTop;
+
+	// // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+	// function addSticky() {
+	// 	if (window.pageYOffset >= sticky) {
+	// 		navbar.classList.add("sticky")
+	// 	} else {
+	// 		navbar.classList.remove("sticky");
+	// 	}
+	// }
+	const hamburger = document.querySelector(".hamburger");
+    const navMenu = document.querySelector('.nav-menu');
+    hamburger.addEventListener('click', () =>{
+        hamburger.classList.toggle('active');
+        navMenu.classList.toggle('active');
+    });
+
+    document.querySelectorAll('.nav-link').forEach(n => n.
+        addEventListener('click', () =>{
+            hamburger.classList.remove('active');
+            navMenu.classList.remove('active');
+        }));
+
+
+	let navbar = document.getElementById("navbar");
+	let navPos = navbar.getBoundingClientRect().top;
+	window.addEventListener("scroll", e => {
+		let viewportHeight = window.innerHeight;
+		let scrollPos = window.scrollY;
+		if (scrollPos > navPos) {
+			navbar.classList.add('sticky');
+			content.classList.add('navbarOffsetMargin');
+		} else {
+		navbar.classList.remove('sticky');
+		content.classList.remove('navbarOffsetMargin');
+		}
+	  });
 });
 function scrollToIndustry() {
 	const container = $('html, body')
@@ -18,5 +61,6 @@ function scrollToIndustry() {
     	+ container.scrollTop();
 	container.animate({
 		scrollTop: position
-  });
+	}, 200);
+	$(scrollTo).addClass("focusBorder");
 }
